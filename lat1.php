f08acec6154c92d5a0fed353fc4e972b5bb9fa95
imeldaade <imeldaade@gmail.com>
2017-03-22 19:33:51 +0700

    Delete lat1.php

--- lat1.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $json_string = file_get_contents("http://api.wunderground.com/api/099d6d8652fcd430/geolookup/conditions/q/indonesia/bekasi.json");
-  $parsed_json = json_decode($json_string);
-  $location = $parsed_json->{'location'}->{'city'};
-  $weather = $parsed_json->{'current_observation'}->{'weather'};
-  echo "The weather of ${location} is: ${weather}\n";
-?>
